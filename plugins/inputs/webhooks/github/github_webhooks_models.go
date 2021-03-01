93a7871fefec970d340a9d943bd839a459601c76
Marcelo Milera <marcelo@able.co>
2021-02-28 23:35:31 -0500

    Add Dockerfile to build an image with a custom version of Telegraf

--- plugins/inputs/webhooks/github/github_webhooks_models.go
+++ plugins/inputs/webhooks/github/github_webhooks_models.go
@@ -744,7 +744,7 @@ func (s WorkflowRunEvent) NewMetric() telegraf.Metric {
 	f := map[string]interface{}{
 		"created_at":  		createdAt.Unix(),
 		"updated_at":  		updatedAt.Unix(),
-		"total_duration": 	updatedAt.Sub(createdAt).Seconds,
+		"total_duration": 	updatedAt.Sub(createdAt).Seconds(),
 	}
 	m, err := metric.New(meas, t, f, createdAt)
 	if err != nil {
